75c05b58e2db2d30660cf2d34496bede5aea0dbb
Gabriel Demetrios Lafis <gabrieldemetrios@gmail.com>
2025-10-07 08:44:34 -0400

    Add R package installation guide

--- INSTALL.R
+++ INSTALL.R
@@ -1,2 +1,3 @@
-# R packages required:
-# install.packages(c('forecast', 'tseries', 'ggplot2', 'quantmod'))
+# R packages required for time series forecasting
+# Run this in R console:
+install.packages(c('forecast', 'tseries', 'ggplot2', 'quantmod', 'zoo', 'xts'))
